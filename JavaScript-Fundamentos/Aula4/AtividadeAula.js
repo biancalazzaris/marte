7c6f7d5e9da99353c9cd937bf38058cb4b1ac810
bianca.lazzaris <bianca.lazzaris@gmail.com>
2021-08-29 15:38:17 -0300

    aula4

---
+++ JavaScript-Fundamentos/Aula4/AtividadeAula.js
@@ -0,0 +1,21 @@
+let fullName ={
+  name: "Bianca",
+  lastname: "Lazzaris",
+  age: 25
+};
+
+fullName.name;
+fullName.lastname;
+
+let terreno = { 
+ lados: 4,
+ area: function(lado) {
+   return lado * lado;
+ }
+}
+terreno.area(5)
+
+totalVitorias = 20,
+totalEmpates = 10,
+totalDerrotas = 5,
+
